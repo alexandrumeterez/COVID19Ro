9aee915b2a0fc78330721e438d8c7a4d58938d9a
Alexandru Meterez <alexandrumeterez@gmail.com>
2020-03-24 14:52:20 +0200

    fixed csv sources

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from backend.fetch import *
 from config.config import *
 import atexit
+from datetime import datetime, timedelta
 from utils.plot_utils import *
-from datetime import datetime
 from bokeh.layouts import column
 from bokeh.resources import INLINE
 from bokeh.embed import components
@@ -15,10 +15,10 @@ from models.data import prepare_data
 from utils.extra import mongodb_to_dict
 from pymongo import MongoClient
 import os
+from bokeh.models.widgets import Tabs, Panel
 
-# client = MongoClient(os.environ['MONGODB_URI'], retryWrites=False)
-client = MongoClient()
-db = client.covid
+client = MongoClient(os.environ['MONGODB_URI'], retryWrites=False)
+db = client.get_default_database()
 
 app = Flask(__name__, template_folder="templates", static_folder='static')
 
@@ -26,8 +26,10 @@ app = Flask(__name__, template_folder="templates", static_folder='static')
 def update_models():
     ro_data = mongodb_to_dict(db.cases.find({}))
     indices, confirmed_cases = prepare_data(ro_data)
-    logistic_values, _ = curve_fit(logistic_model, indices, confirmed_cases, p0=[2, 58, 10000])
-    exponential_values, _ = curve_fit(exponential_model, indices, confirmed_cases, p0=[1, 1, 1])
+    logistic_values, _ = curve_fit(logistic_model, indices, confirmed_cases, p0=[3, 60, 50000],
+                                   bounds=(0, [5., 365., 1000000]))
+    exponential_values, _ = curve_fit(exponential_model, indices, confirmed_cases, p0=[1, 1, 1],
+                                      bounds=(0, [5., 365., 1000000]))
     a, b, c = logistic_values[0], logistic_values[1], logistic_values[2]
     sol = int(fsolve(lambda x: logistic_model(x, a, b, c) - int(c), b))
 
@@ -50,9 +52,6 @@ def update_data():
 
     cases = db.cases
     for case in cases_list:
-        print(case[0])
-        print(case[1]['confirmed'])
-        print(case[1]['deaths'])
         result = cases.update_one({'_id': case[0]}, {
             '$set': {'confirmed': case[1]['confirmed'], 'deaths': case[1]['deaths']}},
                                   upsert=True)
@@ -64,9 +63,31 @@ def update_data():
 
 def update_plots():
     ro_data = mongodb_to_dict(db.cases.find({}))
-    overlapped_plot = generate_overlap(ro_data, "orange", "red")
-    confirmed_cases_plot = generate_plot(ro_data, "confirmed", "orange", "gold")
-    deaths_cases_plot = generate_plot(ro_data, "deaths", "salmon", "red")
+
+    overlapped_plot = Tabs(tabs=[
+        Panel(
+            child=column(generate_overlap(ro_data, "orange", "red", 'linear'), sizing_mode="stretch_width"),
+            title="Liniar"),
+        Panel(child=column(generate_overlap(ro_data, "orange", "red", 'log'), sizing_mode="stretch_width"),
+              title="Logaritmic")
+    ], sizing_mode="stretch_width")
+
+    confirmed_cases_plot = Tabs(tabs=[
+        Panel(
+            child=column(generate_plot(ro_data, "confirmed", "orange", "gold", 'linear'), sizing_mode="stretch_width"),
+            title="Liniar"),
+        Panel(child=column(generate_plot(ro_data, "confirmed", "orange", "gold", 'log'), sizing_mode="stretch_width"),
+              title="Logaritmic")
+    ], sizing_mode="stretch_width")
+
+    deaths_cases_plot = Tabs(tabs=[
+        Panel(
+            child=column(generate_plot(ro_data, "deaths", "salmon", "red", 'linear'), sizing_mode="stretch_width"),
+            title="Liniar"),
+        Panel(child=column(generate_plot(ro_data, "deaths", "salmon", "red", 'log'), sizing_mode="stretch_width"),
+              title="Logaritmic")
+    ], sizing_mode="stretch_width")
+
     col_layout_index = column(overlapped_plot, confirmed_cases_plot, deaths_cases_plot,
                               sizing_mode="stretch_width")
     script_index, div_index = components(col_layout_index)
@@ -123,8 +144,13 @@ def predictions():
     last_updated = db.last_updated.find_one({'_id': 1})['last_updated']
     js_resources = INLINE.render_js()
     css_resources = INLINE.render_css()
+    params = db.models_params.find_one({'_id': 1})
+    sol = params['sol']
+    c = params['logistic_values'][2]
+    end_date = datetime.strptime("22.01.2020", "%d.%m.%Y") + timedelta(sol)
+    end_date_str = end_date.strftime("%d.%m.%Y")
     return render_template("predictions.html", js_resources=js_resources, css_resources=css_resources, script=script,
-                           div=div, last_updated=last_updated)
+                           div=div, last_updated=last_updated, end_date_str=end_date_str, n_confirmed=int(c))
 
 
 @app.route("/sources")
@@ -140,4 +166,4 @@ scheduler.start()
 if __name__ == '__main__':
     atexit.register(lambda: scheduler.shutdown())
     update_data()
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
--- config/config.py
+++ config/config.py
@@ -1,3 +1,3 @@
-UPDATE_INTERVAL = 3
+UPDATE_INTERVAL = 1
 URL_CONFIRMED = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 URL_DEATHS = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
